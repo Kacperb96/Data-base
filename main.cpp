e575935913b411a01ab359a80031c4b5d7567b45
Kacperb96 <kacperb1996@icloud.com>
2022-05-08 19:34:16 +0200

    Home screen added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+void intro();
 
 int main(){
-    std::cout << "Hello from main\n";
+    system("clear");
+    char ch;
+    intro();
 
+    do{
+        std::cout << "\n\n\n\t\t\t\tHOME SCREEN\n\n\n";
+        std::cout << "\n\n\t1. Result Calculation System";
+        std::cout << "\n\n\t2. Student Database Management System";
+        std::cout << "\n\n\t3. Exit";
+        std::cout << "\n\n\n\n\n\tPlease Select Your Option (1-3) ";
+        std::cin >> ch;
+
+        switch(ch){
+            case '1':
+                 //shows Result Calculation System
+                 break;  
+            case '2':
+                 //shows Student Database Management System Code
+                 break;
+            case '3':
+                 //user wants to exit the system
+                 break;
+            default: std::cout<<"\a";
+  }
+    }while(ch != '3');
+    std::cin.get();
     return 0;
+}
+
+void intro(){
+    std::cout << "\n\n******** WELCOME TO STUDENT DATABASE MANAGEMENT AND RESULT CALCULATION SYSTEM *******\n\n";
+
 }
